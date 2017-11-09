cb0411a50f391d120d847809784481d2c26843b6
Andreas Schroeder <andreas@a-netz.de>
2017-11-09 13:13:14 +0800

    Update of jquery. Misc code changes.

--- web/code.js
+++ web/code.js
@@ -7,47 +7,47 @@ var handlers = {
 
 /** Init to connect to MQTT broker. */
 function MQTTconnect() {
-    if (typeof path == "undefined") {
-    	path = '/mqtt';
-    }
     mqtt = new Paho.MQTT.Client(
-    		host,
-    		port,
-    		path,
-    		"web_" + parseInt(Math.random() * 100, 10)
+    	config['broker'],
+    	config['port'],
+    	config['path'],
+    	"web_" + parseInt(Math.random() * 100, 10)
     );
+    
     var options = {
         timeout: 3,
-        useSSL: useTLS,
-        cleanSession: cleansession,
+        useSSL: config['useTLS'],
+        cleanSession: config['cleansession'],
         onSuccess: onConnect,
-        onFailure: function (message) {
-            $('#status').val("Connection failed: " + message.errorMessage + "Retrying");
-            setTimeout(MQTTconnect, reconnectTimeout);
-        }
+        onFailure: onFailure
     };
+
+    if ('username' in config) {
+        options.userName = config['username'];
+        options.password = config['password'];
+    }
 
     mqtt.onConnectionLost = onConnectionLost;
     mqtt.onMessageArrived = onMessageArrived;
 
-    if (username != null) {
-        options.userName = username;
-        options.password = password;
-    }
-    console.log(
-        "Host=" + host
-            + ", port=" + port
-            + ", path=" + path
-            + " TLS = " + useTLS
-            + " username=" + username
-            + " password=" + password);
+    /* pseudo-secure, as password is contained in config file. */
+    cfg2 = config;
+    cfg2['password'] = '***';
+    console.log(cfg2);
     
     mqtt.connect(options);
 }
 
+
+function onFailure(message) {
+    $('#status').val("Connection failed: " + message.errorMessage + "Retrying");
+    setTimeout(MQTTconnect, reconnectTimeout);
+}
+    
+
 function onConnect() {
-    $('#status').val('Connected to ' + host + ':' + port + path);
-    $('#status').css('background-color', 'green');
+    $('#status').val('Connected to ' + mqtt.host + ':' + mqtt.port + mqtt.path);
+    $('#status').css('background-color', 'lightgreen');
 
     // Connection succeeded; subscribe to our topic
 
@@ -67,29 +67,41 @@ function onConnect() {
  */
 function onConnectionLost(response) {
     setTimeout(MQTTconnect, reconnectTimeout);
+    
     $('#status').val("connection lost: " + response.errorMessage + ". Reconnecting...");
     $('#status').css('background-color', 'red');
 };
 
-function onMessageArrived(message) {
 
+function onMessageArrived(message) {
     var topic = message.destinationName;
     var payload = message.payloadString;
 
     var tparts = topic.split('/');
 
-
     var key = tparts[3];
     if(key in handlers) {
-        handlers[tparts[3]](payload);
+        handlers[key](topic, payload);
     } else {
         console.log('Got unhandled topic ' + topic);
     }
 };
 
-function handleLoadAvg(payload) {
-    $('#loadavg').text(payload);
+
+function getRgb(val) {
+    if(val < 0.0) val = 0;
+    if(val > 1.0) val = 1.0;
     
+    var r = val * 255;
+    var g = (1.0 - val) * 255;
+    
+    return 'rgb(' + r + ',' + g + ',0)';
+}
+
+
+function handleLoadAvg(topic, payload) {
+    $('#loadavg').html(payload);
+    $('#loadavg').css('background-color', getRgb(payload));
 }
 
 $(document).ready(function() {
